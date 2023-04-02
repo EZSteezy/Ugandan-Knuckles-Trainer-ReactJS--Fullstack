acc7162c27358fa342a43543cf0b7d823858bde7
Steven <sdeanjr000@gmaail.com>
2023-04-02 18:02:17 -0500

    reversion

--- src/express.js
+++ src/express.js
@@ -1,9 +1,7 @@
-'use strict';
-const dotenv = require('dotenv');
-dotenv.config();
 const express = require('express');
 const { Pool } = require('pg');
 const app = express();
+const port = process.env.PORT || 3001;
 
 
 app.use(express.static('public'));
@@ -17,16 +15,15 @@ app.use((req, res, next) => {
   next();
 });
 
-const DB_HOST = process.env.DATABASE_HOST || 'dpg-cgkt4tm4dad69r4o7270-a';
-
 const pool = new Pool({
   user: 'scores_db_user',
-  host: DB_HOST,
+  host: process.env.DATABASE_HOST || 'dpg-cgkt4tm4dad69r4o7270-a',
   database: 'scores_db',
   password: 'fvlnOLMLXjtLc0clcgkgKsTXSsbFI28G',
   port: 5432,
 });
 
+// GET route to handle request for scores
 app.get('/api/scores', (req, res, next) => {
   pool.query('SELECT * FROM scores', (err, result) => {
     if (err) {
@@ -38,7 +35,12 @@ app.get('/api/scores', (req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+// error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('Something went wrong!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
