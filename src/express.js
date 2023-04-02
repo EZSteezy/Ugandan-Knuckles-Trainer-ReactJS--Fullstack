2cb3b367dbc5f64f3bedbd0182c434dfe6eccdab
Steven <sdeanjr000@gmaail.com>
2023-04-02 18:12:09 -0500

    somehow deleted my port line

--- src/express.js
+++ src/express.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import pg from 'pg';
-
+const port = process.env.PORT || 3000;
 const app = express();
 
 const DB_HOST = process.env.DATABASE_HOST || 'dpg-cgkt4tm4dad69r4o7270-a';
